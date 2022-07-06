2f7036ae64f657a24e411cbd85ed40c046465bc1
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-07 00:08:39 +0300

    Create A method updates posts | returns the 5 most recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   belongs_to :user, foreign_key: :author_id
   has_many :likes
   has_many :comments
+
+  def update_post_counter
+    user.increment!(:posts_counter)
+  end
+
+  def most_recent_five_comments
+    comments.order!(created_at: :desc).limit(5)
+  end
 end
