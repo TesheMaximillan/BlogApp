efb2b8674c65363a5018a37fac04ffd9f6e842b8
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-07 21:16:18 +0300

    Add validation to comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user, foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
+  validates :text, presence: true, length: { minimum: 3, maximum: 255 }
 
   def update_comment_counter
+    post.comments_counter = 0 if post.comments_counter.nil?
     post.increment!(:comments_counter)
   end
 end
