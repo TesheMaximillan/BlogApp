05ac5177ea61904825bac493f857058aa665a50f
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-11 23:22:44 +0300

    Test posts controller | GET /users/:user_id/posts/:id

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -12,4 +12,16 @@ describe 'Posts', type: :request do
       expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'check the status is successful' do
+      get user_post_path(1, 1)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'check the body has a placeholder' do
+      get user_post_path(1, 1)
+      expect(response.body).to include('Here is a single post with a collection of comments')
+    end
+  end
 end
