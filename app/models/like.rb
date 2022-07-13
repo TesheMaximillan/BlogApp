fafdae2010818d39711a31105f9cb7878fe1ed14
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-13 18:46:48 +0300

    Add after_save to update the counters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ActiveRecord::Base
   belongs_to :user, foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
+  after_save :update_likes_counter
 
   def update_likes_counter
     post.likes_counter = 0 if post.likes_counter.nil?
