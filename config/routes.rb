fb20cb2ee0581c7836027c2ead3156ef3b671809
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-15 02:33:03 +0300

    Modify routes for post action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create]
+    post "posts/:id/comments" => "posts#create_comment", as: :create_comment
+    post "posts/:id/likes" => "posts#create_like", as: :create_like
   end
 end
