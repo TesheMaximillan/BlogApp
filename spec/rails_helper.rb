470e7d0a52efd50e3f8b94a8d5c2c3d104b97bc3
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-19 22:07:46 +0300

    Fix linters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,7 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  RSpec.describe "Signing in", js: true do
+  RSpec.describe 'Signing in', js: true do
     # tests that actually execute JavaScript
   end
 end
