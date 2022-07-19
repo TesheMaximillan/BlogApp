41599cea8e316d77b76729d7feec660bdf25d352
dewslyse <dewslyse@gmail.com>
2022-07-19 18:58:05 +0000

    Add integration test for title and user in post

---
+++ spec/views/post_show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Post show page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Tomas', photo: 'https://usp.com/pto.png', bio: 'Web developer')
+    @post = Post.create(user: @user, title: 'Dev in 2020', text: 'How to be a dev in 2020')
+    @comment1 = Comment.create(user: @user, post: @post, text: 'This is comment 1')
+    @comment2 = Comment.create(user: @user, post: @post, text: 'This is comment 2')
+    Like.create(user: @user, post: @post)
+
+    visit user_post_path(@user, @post)
+  end
+
+  it 'displays the post\'s title' do
+    expect(page).to have_content('Dev in 2020')
+  end
+
+  it 'displays the author\'s username' do
+    expect(page).to have_content(@user.name)
+  end
+end
