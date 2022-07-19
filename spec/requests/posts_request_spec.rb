34a26b64e862b68d464984e6ec01e6f20dd891de
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-19 17:50:15 +0300

    Modify rails spec

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -7,11 +7,6 @@ describe 'Posts', type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it 'check the body has a placeholder' do
-      get user_posts_path(1)
-      expect(response.body).to include('Here is a list of posts for a given user')
-    end
-
     it 'render the correct template' do
       get user_posts_path(1)
       expect(response).to render_template('posts/index')
@@ -22,11 +17,6 @@ describe 'Posts', type: :request do
     it 'check the status is successful' do
       get user_post_path(1, 1)
       expect(response).to have_http_status(200)
-    end
-
-    it 'check the body has a placeholder' do
-      get user_post_path(1, 1)
-      expect(response.body).to include('Here is a single post with a collection of comments')
     end
 
     it 'render the correct template' do
