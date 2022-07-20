b5dbc770cea59362f0368a94a4e0141ba16d3c89
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-20 22:52:36 +0300

    Add email validation in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   has_many :comments
@@ -8,6 +9,8 @@ class User < ActiveRecord::Base
   has_many :likes
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   def most_recent_three_posts
     posts.order!(created_at: :desc).limit(3)
