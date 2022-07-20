7b2dd5ef034bf1d5f8a4dc858de62e59ce776c19
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-20 18:26:27 +0300

    Add a route for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "home#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create]
     post "posts/:id/comments" => "posts#create_comment", as: :create_comment
